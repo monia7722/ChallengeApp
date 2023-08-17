0ff10a37a577cbe9db144212174579f46124c0b7
Monika <monia.woz7777@gmail.com>
2023-08-17 18:25:04 +0200

    Zadanie domowe dzień 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
+//k - kobieta, m - męzczyzna
+
+string name = "Ewa";
+string gender = "k";
+var age = 33;
+string nationality = "Polka";
+
+if (age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (gender != "k")
+{
+    Console.WriteLine("Niepełnoletni męzczyzna");
+}
+else if (name == "Ewa" && nationality == "Polka")
+{
+    Console.WriteLine ("Ewa jest Polką");
+}
